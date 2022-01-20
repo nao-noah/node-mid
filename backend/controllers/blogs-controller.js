2d3071b007c6a14f03cf935cf846b519f21151cd
nao-noah <naomooo.official@gmail.com>
2022-01-19 21:52:17 -0800

    done

---
+++ backend/controllers/blogs-controller.js
@@ -0,0 +1,61 @@
+import express from "express";
+import db from "../db";
+const router = express.Router();
+
+router.get("/", async (req, res) => {
+  const sql = `
+    SELECT * FROM blogs
+  `;
+
+  db.query(sql, (err, result) => {
+    res.status(200).json({ blogs: result });
+  });
+});
+
+router.post("/", async (req, res) => {
+  const {
+    body: { content },
+  } = req;
+
+  const sql = `
+    INSERT INTO blogs (content) VALUES (?)
+  `;
+
+  db.query(sql, [content], (err, result) => {
+    res.status(201);
+  });
+});
+
+router.patch("/:id", async (req, res) => {
+  const {
+    query: { id },
+    body: { content },
+  } = req;
+
+  const sql = `
+    UPDATE blogs
+    SET content = ?
+    WHERE id = ?
+  `;
+
+  db.query(sql, [content, id], (err, result) => {
+    res.status(200);
+  });
+});
+
+router.delete("/:id", async (req, res) => {
+  const {
+    query: { id },
+  } = req;
+
+  const sql = `
+    DELETE FROM blogs
+    WHERE id = ?
+  `;
+
+  db.query(sql, [id], (err, result) => {
+    res.status(204);
+  });
+});
+
+module.exports = router;
