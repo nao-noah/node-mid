e56255484926195ab6d5ae41f1d4e38a85945708
nao-noah <naomooo.official@gmail.com>
2022-01-19 21:56:42 -0800

    done

---
+++ backend/controllers/blogs/likes-controller.js
@@ -0,0 +1,34 @@
+import express from "express";
+import db from "../../db";
+const router = express.Router();
+
+router.post("/", async (req, res) => {
+  const {
+    query: { blogId },
+  } = req;
+
+  const sql = `
+    INSERT INTO likes (blog_id) VALUES (?)
+  `;
+
+  db.query(sql, [blogId], (err, result) => {
+    res.status(201);
+  });
+});
+
+router.delete("/", async (req, res) => {
+  const {
+    query: { blogId },
+  } = req;
+
+  const sql = `
+    DELETE FROM blogs
+    WHERE blogId = ?
+  `;
+
+  db.query(sql, [blogId], (err, result) => {
+    res.status(204);
+  });
+});
+
+module.exports = router;
