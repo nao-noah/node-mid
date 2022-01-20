5c566d14e3e6c59456b3c836e24b212010087762
nao-noah <naomooo.official@gmail.com>
2022-01-19 23:40:57 -0800

    add bp

--- backend/app.js
+++ backend/app.js
@@ -2,9 +2,12 @@ const express = require("express");
 const blogsController = require("./controllers/blogs-controller");
 const commentsController = require("./controllers/blogs/comments-controller");
 const likesController = require("./controllers/blogs/likes-controller");
+const bodyParser = require("body-parser");
 
 const app = express();
 const port = 8080;
+
+app.use(bodyParser);
 
 app.use((req, res, next) => {
   res.header("Access-Control-Allow-Origin", "*");
